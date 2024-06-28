7b939bb1b894f7ebe6fe1571994d58a78da687dc
nanami-tomoe <npnp123npnp123@gmail.com>
2024-06-29 00:15:05 +0900

    feat: gradle 버전 변경 - 8.5 -> 8.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.104.Final:osx-aarch_64") // MacOS Silicon 라이브러리 누락 문제
 }
 
 tasks.withType<Test> {
