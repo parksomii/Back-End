554f1837c9d23e983ad1c002ad29d9a81990a019
parksomii <somi020605@gmail.com>
2024-06-30 22:25:15 +0900

    feat: domain, controller, serivce, repository

---
+++ src/main/java/edu/allinone/sugang/domain/Lecture.java
@@ -0,0 +1,39 @@
+package edu.allinone.sugang.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Lecture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int lectureNumber;
+    private String lectureRoom;
+    private String lectureHours;
+    private int totalCapacity;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id")
+    private Professor professor;
+
+    @ManyToOne
+    @JoinColumn(name = "subject_id")
+    private Subject subject;
+
+    @OneToMany(mappedBy = "lecture")
+    private List<Enrollment> enrollments;
+
+    /*
+    @OneToMany(mappedBy = "lecture")
+    private List<Schedule> schedules;
+    */
+}
