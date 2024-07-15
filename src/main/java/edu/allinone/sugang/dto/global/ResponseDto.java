e8d7eb56a0bb7021a949e72de3c7d0513eb9230e
nanami-tomoe <npnp123npnp123@gmail.com>
2024-07-15 10:38:53 +0900

    feat: 공통 ResponseDto 생성

--- src/main/java/edu/allinone/sugang/dto/global/ResponseDto.java
+++ src/main/java/edu/allinone/sugang/dto/global/ResponseDto.java
@@ -1,36 +1,25 @@
 package edu.allinone.sugang.dto.global;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-import java.time.LocalDateTime;
-
-/**
- * Controller에서 최종적으로 내보내는 Dto에 대한 형태를 통일한다.
- * 응답 시 처리 결과 뿐만이 아닌 status, message, timestamp에 대한 값을 추가로 내보낸다
- */
 @NoArgsConstructor
 @Getter
 public class ResponseDto<T> {
     private Integer status;
     private String message;
     private T data;
-    private LocalDateTime timestamp = LocalDateTime.now();
 
-    @Builder
-    public ResponseDto(Integer status, String message, T data) {
-        this.status = status;
+    public ResponseDto(String message, T data) {
+        this.status = HttpStatus.OK.value();
         this.message = message;
         this.data = data;
     }
 
-    public HttpStatus getHttpStatus() {
-        try{
-            return HttpStatus.valueOf(status);
-        }catch (IllegalArgumentException e){
-            return HttpStatus.OK;
-        }
+    public ResponseDto(Integer status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
     }
 }
