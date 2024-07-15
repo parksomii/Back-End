d2cad50412cb82e798f8b7325e25583914617622
nanami-tomoe <npnp123npnp123@gmail.com>
2024-07-15 10:37:42 +0900

    feat: EnrollmentController 추가

--- src/main/java/edu/allinone/sugang/controller/EnrollmentController.java
+++ src/main/java/edu/allinone/sugang/controller/EnrollmentController.java
@@ -4,10 +4,9 @@ import edu.allinone.sugang.dto.global.ResponseDto;
 import edu.allinone.sugang.dto.request.EnrollmentRequestDto;
 import edu.allinone.sugang.service.EnrollmentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/enrollment")
@@ -20,10 +19,31 @@ public class EnrollmentController {
      * 수강 신청 하기
      */
     @PostMapping("/enroll")
-    public ResponseDto<?> enroll(
-            @RequestBody EnrollmentRequestDto requestDto
-    ) {
-        ResponseDto<?> responseDto = enrollmentService.enroll(en);
+    @ResponseStatus(HttpStatus.CREATED) // 상태를 201 Created로 설정
+    public ResponseDto<EnrollmentRequestDto> enroll(@RequestBody EnrollmentRequestDto requestDto) {
+       try {
+            enrollmentService.enroll(requestDto.getStudentId(), requestDto.getLectureId());
+            return new ResponseDto<>(HttpStatus.CREATED.value(), "신청 완료", requestDto);
+        } catch (IllegalArgumentException e) {
+            return new ResponseDto<>(HttpStatus.BAD_REQUEST.value(), e.getMessage(), requestDto);
+        } catch (Exception e) {
+           return new ResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), requestDto);
+       }
     }
 
+    /**
+     * 수강 신청 취소 하기
+     */
+    @DeleteMapping("/{studentId}/{lectureId}")
+    public ResponseDto<EnrollmentRequestDto> cancel(@PathVariable Integer studentId, @PathVariable Integer lectureId) {
+        try {
+            enrollmentService.cancel(studentId, lectureId);
+            EnrollmentRequestDto requestDto = new EnrollmentRequestDto(studentId, lectureId);
+            return new ResponseDto<>(HttpStatus.OK.value(), "취소 완료", requestDto);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
 }
