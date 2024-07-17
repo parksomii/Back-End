be573f29ba4714431e78f98b391bb79995da1da6
nanami-tomoe <npnp123npnp123@gmail.com>
2024-07-17 23:56:39 +0900

    feat: EnrollmentRepository 리스트 반환 메서드 추가

--- src/main/java/edu/allinone/sugang/repository/EnrollmentRepository.java
+++ src/main/java/edu/allinone/sugang/repository/EnrollmentRepository.java
@@ -9,4 +9,7 @@ import java.util.Optional;
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
     // 학생id와 강의id로 수강 신청 정보 가져오기
     Optional<Enrollment> findByStudentIdAndLectureId(Integer studentId, Integer lectureId);
+
+    // 학생id로 수강 신청 내역 리스트 가져오기
+    List<Enrollment> findByStudentId(Integer studentId);
 }
