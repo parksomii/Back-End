a4a34f4f9162a7ee9eebc9de2fd68fa8ae143ff4
nanami-tomoe <npnp123npnp123@gmail.com>
2024-07-22 01:04:22 +0900

    feat: LectureSummaryDTO 생성 - 과목 이름으로 강의 검색 할 때 반환값 - 강의id, 과목 이름, 강의 시간 DTO 반환

---
+++ src/main/java/edu/allinone/sugang/dto/response/LectureSummaryDTO.java
@@ -0,0 +1,22 @@
+package edu.allinone.sugang.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.List;
+
+/**
+ * 강의 명으로 조회 시 강의 목록 반환 DTO
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LectureSummaryDTO {
+    private Integer lectureId;
+    private String subjectName;
+    private List<LectureTimeDTO> lectureTimes;
+}
