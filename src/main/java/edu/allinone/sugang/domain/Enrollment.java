34784c97c602b2254cdfeed2e32407091a25cbd3
parksomii <somi020605@gmail.com>
2024-08-02 19:37:07 +0900

    feat: Enrollment에 고유 제약 조건 재정의
    - 기존 고유 제약 조건 인덱스 삭제
    - student_id와 lecture_id에 대한 새로운 고유 제약 조건 추가

--- src/main/java/edu/allinone/sugang/domain/Enrollment.java
+++ src/main/java/edu/allinone/sugang/domain/Enrollment.java
@@ -6,6 +6,12 @@ import lombok.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "enrollment",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"student_id", "lecture_id"})
+        }
+)
 public class Enrollment {
     /* -------------------------------------------- */
     /* -------------- Default Column -------------- */
@@ -18,11 +24,11 @@ public class Enrollment {
     /* -------------- Relation Column ------------- */
     /* -------------------------------------------- */
     @ManyToOne
-    @JoinColumn(name = "student_id")
+    @JoinColumn(name = "student_id", nullable = false)
     private Student student;
 
     @ManyToOne
-    @JoinColumn(name = "lecture_id")
+    @JoinColumn(name = "lecture_id", nullable = false)
     private Lecture lecture;
 
     /* -------------------------------------------- */
